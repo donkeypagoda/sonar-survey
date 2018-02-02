f0234b7c59c6d31c188d982dd04e6ea3cc8e41d8
donkeypagoda <christopherallenstewart@gmail.com>
2018-02-02 12:47:07 -0800

    couple of things

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Question from './components/question.js'
 
 class App extends Component {
   render() {
     return (
       <test>
       <div className="App">Hello World
-
       </div>
       </test>
     );
---
+++ src/components/question.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Question extends React.Component {
+  render () {
+  return(
+
+
+      <div>HAYO!
+
+      </div>
+
+  )
+}
+}
+
+export default Question
