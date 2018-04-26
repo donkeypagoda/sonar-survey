b18c57bc315719da8e03f8865bfb3a9f9ad38cae
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-26 13:47:10 -0700

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 
-import Question from './components/question.js'
-
 class App extends Component {
   render() {
     return (
-      <test>
       <div className="App">Hello World
       </div>
-      <Question />
-      </test>
     );
   }
 }
