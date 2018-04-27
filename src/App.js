77f685d2c1fd4495942241aec476afc200af0f6f
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-27 14:51:52 -0700

    landing page, base component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch } from "react-router-dom"
 import "./App.css"
+import makeTakeOrViewSurvey from "./components/makeTakeOrViewSurvey"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">Hello World
+      <div className="App">
+        <div className="header">
+        </div>
+
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={makeTakeOrViewSurvey} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
