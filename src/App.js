cd8930ce609c0a7ea0a3b9ea77d5e14275ad1a7b
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-27 14:58:38 -0700

    building router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch } from "react-router-dom"
 import "./App.css"
-import makeTakeOrViewSurvey from "./components/makeTakeOrViewSurvey"
+import MakeTakeOrViewSurvey from "./components/makeTakeOrViewSurvey"
+import SurveyList from "./components/surveyList"
+import NewSurvey from "./components/newSurvey"
 
 class App extends Component {
   render() {
@@ -12,7 +14,9 @@ class App extends Component {
 
         <BrowserRouter>
           <Switch>
-            <Route path="/" component={makeTakeOrViewSurvey} />
+            <Route path="/" component={MakeTakeOrViewSurvey} />
+            <Route path="/survey-list" component={SurveyList} />
+            <Route path="/new-survey" component={NewSurvey} />
           </Switch>
         </BrowserRouter>
       </div>
---
+++ src/components/makeTakeOrViewSurvey.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+import {Link} from "react-router-dom"
+
+class MakeTakeOrViewSurvey extends Component {
+
+  render(){
+    return (
+      <div>
+        Would you like to Make, Take or View the results of a Survey?
+      </div>
+    )
+  }
+}
+
+export default MakeTakeOrViewSurvey
