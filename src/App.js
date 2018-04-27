c8485f4968f664923d96c285c3d48dfc125f792a
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-27 15:07:39 -0700

    basic functionality laid out

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css"
 import MakeTakeOrViewSurvey from "./components/makeTakeOrViewSurvey"
 import SurveyList from "./components/surveyList"
 import NewSurvey from "./components/newSurvey"
+import SignUpOrLogin from "./components/signUpOrLogin"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="header">
+          SonarSurvey - make, take, or view survey results of all kinds!
         </div>
 
         <BrowserRouter>
@@ -17,6 +19,7 @@ class App extends Component {
             <Route path="/" component={MakeTakeOrViewSurvey} />
             <Route path="/survey-list/" component={SurveyList} />
             <Route path="/new-survey/" component={NewSurvey} />
+            <Route path="/login" component={SignUpOrLogin} />
           </Switch>
         </BrowserRouter>
       </div>
---
+++ src/components/signUpOrLogin.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+
+class SignUpOrLogin extends Component{
+
+  render(){
+    return(
+      <div>
+        This will be the login page
+      </div>
+    )
+  }
+}
+
+export default SignUpOrLogin
