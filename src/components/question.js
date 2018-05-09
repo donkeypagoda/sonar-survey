bb7f7183959710672d7f06e37f249fbea51291a3
donkeypagoda <christopherallenstewart@gmail.com>
2018-05-09 14:22:35 -0700

    making answers

--- src/components/question.js
+++ src/components/question.js
@@ -1,4 +1,18 @@
 import React, {Component} from "react"
 
+function Question({question}){
+  return(
+    <div className="question">
+      <div className="container">
+        <div className="prompt">
+          {question.prompt}
+        </div>
+        <div className="answer">
+          {question.answer_array ? <AnswerList answer_array={this.answer_array} /> : null}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Question
