af70eaaa4ea1b98ac713d6a23760631221423691
dude <christopherallenstewart@gmail.com>
2018-05-11 11:27:27 -0700

    component building party

---
+++ src/components/survey.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+
+class Question extends Component{
+
+  render(){
+    <div className="question">
+    QUESTION TEST
+    </div>
+  }
+}
