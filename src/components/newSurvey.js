b7379c2b7bb9d66bc7ee6dbbba0b1ad46b7da547
dude <christopherallenstewart@gmail.com>
2018-05-15 13:50:11 -0700

    building new survey form

--- src/components/newSurvey.js
+++ src/components/newSurvey.js
@@ -2,6 +2,11 @@ import React, {Component} from "react"
 
 class NewSurvey extends Component{
 
+  submitSurvey = e =>{
+    e.preventDefault()
+    console.log(e.target[0].value)
+  }
+
   render(){
     return(
       <div className="newSurvey">
