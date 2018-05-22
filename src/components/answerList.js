b0ebe73f5b63a7b6c0763002c9dd84a9c2f63fba
donkeypagoda <christopherallenstewart@gmail.com>
2018-05-22 15:24:42 -0700

    got questions and answers showing up

--- src/components/answerList.js
+++ src/components/answerList.js
@@ -6,17 +6,28 @@ function AnswerList(props){
   if (props.answer_array != null){
     for (let i = 0; i < props.answer_array.length; i++){
       answerList.push(
-        <div className="answerSelection">
-          <p>Answer Choices:</p>
-            <label>{props.answer_array[i]}</label>
-            <input type="radio" id={props.answer_array[i]} />
+        <div className="answerSelection" key={i}>
+          <label>{props.answer_array[i]}</label>
+          <input type="radio" id={props.answer_array[i]} />
         </div>
       )
     }
   }
   return(
     <div className="answerList">
-      {props.answer_array ? answerList : null}
+      {props.answer_array
+        ?
+        <div>
+          <div>Answer Choices:</div>
+            {answerList}
+        </div>
+        :
+        <div>
+          <form className="answerField">
+            <input type="text" name="answer" />
+          </form>
+        </div>
+      }
     </div>
   )
 }
