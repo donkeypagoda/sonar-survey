32251807103a9941dca20559daab169aeda273b1
donkeypagoda <christopherallenstewart@gmail.com>
2018-05-23 17:17:03 -0700

    modifying answerlist to deal with range

--- src/components/answerList.js
+++ src/components/answerList.js
@@ -4,6 +4,7 @@ function AnswerList(props){
   console.log(props.answer_type)
   let answerList = []
   if (props.answer_type === "multiple_choice"){
+    answerList.push(<div key={-1}>Answer Choices:</div>)
     for (let i = 0; i < props.answer_array.length; i++){
       answerList.push(
         <div className="answerSelection" key={i}>
@@ -23,13 +24,20 @@ function AnswerList(props){
       </div>
     ]
   }
+  else if (props.answer_type === "range"){
+    answerList = [
+      <div className="answerSelection" key={0}>
+        <label>Please Select Between 0 and 10</label>
+        <input type="range" id="range" min="0.0" max="10.0" value="5.0" />
+      </div>
+    ]
+  }
   return(
     <div className="answerList">
       {props.answer_type != "string"
         ?
         <div>
-          <div>Answer Choices:</div>
-            {answerList}
+          {answerList}
         </div>
         :
         <div>
