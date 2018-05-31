dafec396f04c01722c247169c8f22269b3ae8196
donkeypagoda <christopherallenstewart@gmail.com>
2018-05-31 13:57:52 -0700

    test run

--- src/components/surveyResults.js
+++ src/components/surveyResults.js
@@ -4,7 +4,17 @@ import d3 from "d3"
 class SurveyResults extends Component{
   constructor(){
     super()
+    this.state = {
+      data: []
+    }
   }
+
+  render(){
+    <div>
+      TEST
+    </div>
+  }
+
 }
 
 
