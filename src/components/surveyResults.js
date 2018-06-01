99b88183ef899b90ca49fc6f959bd07be3ec6ce5
donkeypagoda <christopherallenstewart@gmail.com>
2018-05-31 17:18:06 -0700

    trying out d3

--- src/components/surveyResults.js
+++ src/components/surveyResults.js
@@ -8,6 +8,18 @@ class SurveyResults extends Component{
       data: []
     }
   }
+  //d3 playground
+  const testData = [
+    { label: "Tacos", count: 15 },
+    { label: "Burgers", count: 12 },
+    { label: "Milkshakes", count: 7 },
+    { label: "Beersh", count: 5 },
+    { label: "Tater Tots", count: 10 },
+  ]
+  const width = 360
+  const height = 360
+  const radius = Math.min(width, height) / 2
+  const color = d3.scaleOrdinal(d3.schemeCategory20b)
 
   render(){
     return (
