ad5f18049edf1df2e45d4d33d67640f730cd4495
donkeypagoda <christopherallenstewart@gmail.com>
2018-06-01 16:00:01 -0700

    it's there, but it's not there

--- src/components/surveyResults.js
+++ src/components/surveyResults.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import d3 from "d3"
+import * as d3 from "d3"
 
 class SurveyResults extends Component{
   constructor(){
@@ -8,23 +8,39 @@ class SurveyResults extends Component{
       data: []
     }
   }
-  //d3 playground
-  const testData = [
-    { label: "Tacos", count: 15 },
-    { label: "Burgers", count: 12 },
-    { label: "Milkshakes", count: 7 },
-    { label: "Beersh", count: 5 },
-    { label: "Tater Tots", count: 10 },
-  ]
-  const width = 360
-  const height = 360
-  const radius = Math.min(width, height) / 2
-  const color = d3.scaleOrdinal(d3.schemeCategory20b)
+  componentDidMount(){
+    //d3 playground
+    const testData = [
+      { label: "Tacos", count: 15 },
+      { label: "Burgers", count: 12 },
+      { label: "Milkshakes", count: 7 },
+      { label: "Beersh", count: 5 },
+      { label: "Tater Tots", count: 10 },
+    ]
+    const width = 360
+    const height = 360
+    const radius = Math.min(width, height) / 2
+    const color = d3.scaleOrdinal().range(['#A60F2B', '#648C85', '#B3F2C9', '#528C18', '#C3F25C'])
+    const svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')')
+
+  }
 
   render(){
     return (
-      <div>
-        TEST
+      <div className="chart">
+        <svg width="360" height="360">
+          <g transform="translate(180,180)">
+            <path d="M0,-180A..." fill="#393b79"></path>
+            <path d="M105.801..." fill="#5254a3"></path>
+            <path d="M171.190..." fill="#6b6ecf"></path>
+            <path d="M-105.80..." fill="#9c9ede"></path>
+          </g>
+        </svg>
       </div>
     )
   }
